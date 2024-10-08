915fd5a7e57b29a1fecaefd95944f0e694f97ef9
MarcusScheffel <46791189+MarcusScheffel@users.noreply.github.com>
2024-10-08 13:01:39 +0200

    Updated files and added new components and images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
+import React from 'react';
 import './App.css';
+import Nav from "./components/Nav"
+import Header from "./components/Header"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <React.Fragment>
+      <Nav />
+      <Main />
+      <Footer />
+    </React.Fragment>
     </div>
   );
 }
